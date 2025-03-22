da44b77df995a07c56a4eed47631e6eba84675bd
Juliano Laurentino <julianolaurentino@gmail.com>
2025-03-21 21:01:52 -0300

    update novas consultas/exercicios

--- app/querys_samples_ex.sql
+++ app/querys_samples_ex.sql
@@ -11,6 +11,7 @@ SELECT
     ,TRIM(REPLACE(PhoneNumber, ' ','-' )) AS PhoneNumber
 FROM Person.PersonPhone;
 
+--Listar os produtos mais vendidos com suas categorias
 SELECT
     PP.Name AS ProductName
     ,PPS.Name AS ProductSubcategory
@@ -20,3 +21,47 @@ INNER JOIN Production.Product PP ON SOD.ProductID = PP.ProductID
 INNER JOIN  Production.ProductSubcategory PPS ON PP.ProductSubcategoryID = PPS.ProductSubcategoryID
 GROUP BY PP.Name, PPS.Name
 ORDER BY OrderQty DESC;
+
+--Encontrar os clientes e o total que gastaram em pedidos
+--Desafio: Liste apenas os clientes que gastaram mais de $5000.
+SELECT 
+    SC.CustomerID
+    ,SC.TerritoryID
+    ,COUNT(SOH.SalesOrderID) AS TotalQty
+    ,SUM(SOH.TotalDue) AS Total
+FROM Sales.Customer SC
+INNER JOIN Sales.SalesOrderHeader SOH ON SC.TerritoryID = SOH.TerritoryID
+WHERE SOH.TotalDue > 5000
+GROUP BY SC.CustomerID, SC.TerritoryID
+ORDER BY Total;
+
+--Listar produtos com preço acima da média
+WITH TotalMedia AS(
+    SELECT
+        Name
+        ,ListPrice
+    FROM Production.Product
+    WHERE ListPrice > 1000
+)
+SELECT *
+FROM TotalMedia
+WHERE ListPrice > (SELECT AVG(ListPrice) FROM Production.Product)
+ORDER BY ListPrice DESC;
+
+--Encontrar os 5 produtos mais vendidos por categoria
+WITH VendasPorProduto AS (
+    SELECT 
+        P.ProductID,
+        P.Name AS Produto,
+        PS.Name AS Categoria,
+        COUNT(SD.SalesOrderID) AS TotalVendas,
+        RANK() OVER (PARTITION BY PS.Name ORDER BY COUNT(SD.SalesOrderID) DESC) AS Ranking
+    FROM Sales.SalesOrderDetail SD
+    JOIN Production.Product P ON SD.ProductID = P.ProductID
+    JOIN Production.ProductSubcategory PS ON P.ProductSubcategoryID = PS.ProductSubcategoryID
+    GROUP BY P.ProductID, P.Name, PS.Name
+)
+SELECT * 
+FROM VendasPorProduto
+WHERE Ranking <= 5
+ORDER BY Categoria, Ranking;
